9fe267dbf3dbaaa18be6ad9ed2a51a7f2819fddd
LFu <lyuwen@users.noreply.github.com>
2022-02-07 02:21:37 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 output=$(echo $3 | sed 's/\.tex$/.pdf/')
 echo "::set-output name=output::$output"
-cd $1 && /usr/local/texlive/2021/bin/x86_64-linux/latexmk $2 $3
+# cd $1 && /usr/local/texlive/2021/bin/x86_64-linux/latexmk $2 $3
